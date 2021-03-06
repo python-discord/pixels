389a71a6d05f76d8fb0c0ec0a16cd567d03d7d3c
Chris <chris.lovering.95@gmail.com>
2021-03-06 16:14:23 +0000

    Remove test endpoint

--- april/april.py
+++ april/april.py
@@ -7,9 +7,3 @@ app = FastAPI()
 async def index() -> dict:
     """Basic hello world endpoint."""
     return {"Message": "Hello!"}
-
-
-@app.get("/test")
-async def test() -> dict:
-    """This is another test endpoint."""
-    return {"Message": "test"}
