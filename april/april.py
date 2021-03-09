0f83e5210fe424302410188c126ed0dcfe1b4efc
Chris <chris.lovering.95@gmail.com>
2021-03-09 18:21:43 +0000

    Create pool connection and give out a connection to each request.

--- april/april.py
+++ april/april.py
@@ -1,9 +1,41 @@
-from fastapi import FastAPI
+import typing as t
+
+import asyncpg
+from fastapi import FastAPI, Request, Response
+
+from april import constants
 
 app = FastAPI()
 
 
+@app.on_event("startup")
+async def startup() -> None:
+    """Create a asyncpg connection pool on startup."""
+    app.state.db_pool = await asyncpg.create_pool(
+        constants.uri
+    )
+
+
+@app.middleware("http")
+async def get_connection_from_pool(request: Request, callnext: t.Callable) -> Response:
+    """Get a connection from the pool for this request."""
+    async with app.state.db_pool.acquire() as connection:
+        request.state.db_conn = connection
+        response = await callnext(request)
+    request.state.db_conn = None
+    return response
+
+
 @app.get("/")
-async def index() -> dict:
+async def index(request: Request) -> Response:
     """Basic hello world endpoint."""
     return {"Message": "Hello!"}
+
+
+@app.get("/get_pixels")
+async def get_pixels(request: Request) -> Response:
+    """Get the current state of all pixels from the db."""
+    return await request.state.db_conn.fetch("""
+    SELECT *
+    FROM current_pixel
+    """)
---
+++ april/constants.py
@@ -0,0 +1,3 @@
+from os import environ
+
+uri = environ.get('DATABASE_URL')
