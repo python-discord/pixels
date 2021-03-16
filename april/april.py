75156758b8404018cce91791be38008866ca4905
lavirlifiliol <adam.hrbac@gmail.com>
2021-03-16 17:22:09 +0100

    Finalize token acquiring
    We may want to make a more friendly frontend for this than just string json.

--- april/april.py
+++ april/april.py
@@ -1,9 +1,12 @@
 import re
+import secrets
 import typing as t
 
 import asyncpg
+from asyncpg import Connection
 from fastapi import FastAPI, Request, Response
 from httpx import AsyncClient
+from jose import jwt
 from pydantic import BaseModel, validator
 from starlette.responses import RedirectResponse
 
@@ -81,9 +84,13 @@ async def get_pixels(request: Request) -> Response:
     return Response(bytes(canvas.cache), media_type="application/octet-stream")
 
 
-@app.get('/get_token')
+@app.get("/get_token")
 async def get_token() -> Response:
-    """Redirect the user to discord authorization, the flow continues in swap_code."""
+    """
+    Redirect the user to discord authorization, the flow continues in swap_code.
+
+    Unlike other endpoints, you should open this one in the browser, since it redirects to a discord website.
+    """
     return RedirectResponse(url=constants.auth_uri)
 
 
@@ -92,39 +99,67 @@ def build_oauth_token_request(code: str) -> t.Tuple[dict, dict]:
     query = dict(
         client_id=constants.client_id,
         client_secret=constants.client_secret,
-        grant_type='authorization_code',
+        grant_type="authorization_code",
         code=code,
         redirect_uri=constants.redirect_uri,
-        scope='identify',
+        scope="identify",
     )
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
     return query, headers
 
 
-@app.get('/swap_code')
-async def swap_code(request: Request) -> dict:
+@app.get("/swap_code", include_in_schema=False)
+async def swap_code(request: Request) -> Response:
     """
     Create the user given the authorization code.
 
     This endpoint is only used as a redirect target from discord.
     Perhaps hide it from the docs.
     """
-    code = request.query_params['code']
+    code = request.query_params["code"]
     async with AsyncClient() as client:
         token_params, token_headers = build_oauth_token_request(code)
-        token = (await client.post(
-            constants.token_url,
-            data=token_params,
-            headers=token_headers
-        )).json()
-        auth_header = {
-            'Authorization': f"Bearer {token['access_token']}"
-        }
+        token = (
+            await client.post(
+                constants.token_url, data=token_params, headers=token_headers
+            )
+        ).json()
+        auth_header = {"Authorization": f"Bearer {token['access_token']}"}
         user = (await client.get(constants.user_url, headers=auth_header)).json()
+        token = await reset_user_token(request.state.db_conn, user["id"])
 
-    return user
+    return RedirectResponse("/show_token?token=" + token)
+
+
+@app.get("/show_token", include_in_schema=False)
+async def show_token(request: Request) -> str:
+    """Take a token from URL and show it."""
+    return request.query_params["token"]
+
+
+async def reset_user_token(conn: Connection, user_id: str) -> str:
+    """
+    Ensure a user exists and create a new token for them.
+
+    If the user already exists, their token is regenerated and the old is invalidated.
+    """
+    # returns None if the user doesn't exist and false if they aren't banned
+    is_banned = await conn.fetchval(
+        "SELECT is_banned FROM users WHERE user_id = $1", int(user_id)
+    )
+    if is_banned:
+        return "You are banned"
+    # 22 long string
+    token_salt = secrets.token_urlsafe(16)
+    async with conn.transaction():
+        await conn.execute(
+            """INSERT INTO users (user_id, key_salt) VALUES ($1, $2)
+            ON CONFLICT (user_id) DO UPDATE SET key_salt=$2;""",
+            int(user_id),
+            token_salt,
+        )
+    jwt_data = dict(id=user_id, salt=token_salt)
+    return jwt.encode(jwt_data, constants.jwt_secret, algorithm="HS256")
 
 
 @app.post("/set_pixel")
--- april/constants.py
+++ april/constants.py
@@ -6,11 +6,12 @@ uri = config("DATABASE_URL")
 
 client_id = config("CLIENT_ID")
 client_secret = config("CLIENT_SECRET")
+jwt_secret = config("JWT_SECRET")
 # starlette already quotes urls, so the url copied from discord ends up double encoded
-auth_uri = config('AUTH_URI', cast=unquote)
-redirect_uri = config('REDIRECT_URI')
-token_url = config('TOKEN_URL', default='https://discord.com/api/oauth2/token')
-user_url = config('TOKEN_URL', default='https://discord.com/api/users/@me')
+auth_uri = config("AUTH_URI", cast=unquote)
+redirect_uri = config("REDIRECT_URI")
+token_url = config("TOKEN_URL", default="https://discord.com/api/oauth2/token")
+user_url = config("TOKEN_URL", default="https://discord.com/api/users/@me")
 
 width = 160
 height = 90
