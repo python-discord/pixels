68df0ec1904b0c3da77f6b0277dd45054c960e53
Matteo Bertucci <matteobertucci2004@gmail.com>
2021-05-09 12:23:52 +0200

    Docs: use a relative path
    This caused issues outside of Docker because /app didn't exist.

--- pixels/utils/docs.py
+++ pixels/utils/docs.py
@@ -1,4 +1,4 @@
 def get_doc(file_name: str) -> str:
     """Fetch and return the given doc file."""
-    with open(f"/app/pixels/docs/{file_name}.md", encoding="UTF-8") as file:
+    with open(f"./pixels/docs/{file_name}.md", encoding="UTF-8") as file:
         return file.read()
