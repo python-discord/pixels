c0d7956427cd383667257d59da7933ce705c75f6
Chris <chris.lovering.95@gmail.com>
2021-06-02 23:12:41 +0100

    Refactor timelapse script to extract and save in one loop

--- scripts/generate_timelapse.py
+++ scripts/generate_timelapse.py
@@ -3,9 +3,8 @@ import logging
 import os
 import shutil
 import typing as t
-from dataclasses import dataclass
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta
-from operator import attrgetter
 from pathlib import Path
 from time import perf_counter
 
@@ -21,9 +20,11 @@ MAX_WIDTH = 240
 MAX_HEIGHT = 135
 
 START_DATE = datetime(2021, 5, 24, 23, 10)
-END_DATE = datetime(2021, 6, 2, 14, 50)
+END_DATE = datetime(2021, 6, 2, 20, 31)
 INTERVAL_DELTA = timedelta(minutes=1)
-FPS = 60
+FPS = 120
+
+_EXECUTOR = ThreadPoolExecutor(10)
 
 log = logging.getLogger()
 format_string = "[%(asctime)s] [%(process)d] [%(levelname)s] %(name)s - %(message)s"
@@ -43,15 +44,14 @@ def datetime_range(start: datetime, end: datetime, delta: timedelta) -> t.Genera
         current += delta
 
 
-@dataclass
-class Snapshot():
-    """Defines the attributes of a Snapshot."""
+def save_image(image_bytes: bytearray, filepath: str) -> None:
+    """Renders and saves an image from raw bytes."""
+    image = Image.frombytes("RGB", (MAX_WIDTH, MAX_HEIGHT), bytes(image_bytes))
+    image = image.resize((1600, 900), Image.NEAREST)
+    image.save(filepath, format="png")
 
-    time: datetime
-    pixels: bytearray
 
-
-async def fetch_one_snapshot(pool: Pool, time: datetime) -> Snapshot:
+async def save_one_snapshot(pool: Pool, time: datetime) -> None:
     """Gets a snapshot of the canvas at the given time."""
     sql = (
         "SELECT PH.x, PH.y, PH.rgb "
@@ -71,26 +71,27 @@ async def fetch_one_snapshot(pool: Pool, time: datetime) -> Snapshot:
             async for record in conn.cursor(sql, time):
                 position = record["y"] * MAX_WIDTH + record["x"]
                 cache[position * 3:(position + 1) * 3] = bytes.fromhex(record["rgb"])
-    return Snapshot(time, cache)
+
+    interval_offset = (time - START_DATE).seconds // INTERVAL_DELTA.seconds
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(_EXECUTOR, save_image, cache, f"timelapse_output/frames/frame{interval_offset}.png")
 
 
-async def get_snapshots() -> t.List[Snapshot]:
+async def get_snapshots() -> None:
     """Fetch snapshots from the db using a connection pool."""
     pool = await DB_POOL
     times = [*datetime_range(START_DATE, END_DATE, INTERVAL_DELTA)]
-
-    snapshots: t.List[Snapshot] = []
 
     batch_size = 100
     total_blocks = (len(times) // batch_size) + 1
     for i in range(total_blocks):
         log.info(f"Running block {i+1} of {total_blocks}")
-        snapshots += await asyncio.gather(*[
-            fetch_one_snapshot(pool, time)
+        await asyncio.gather(*[
+            save_one_snapshot(pool, time)
             for time in times[i*100:(i+1)*100]
         ])
-    return snapshots
 
+Path("timelapse_output/frames").mkdir(parents=True, exist_ok=True)
 
 log.info("Starting DB extract...")
 
@@ -100,25 +101,7 @@ snapshots = loop.run_until_complete(get_snapshots())
 
 log.info(f"DB extract done. Took {perf_counter()-start:.2f}s")
 
-log.info("Transforming raw bytes into images...")
-start = perf_counter()
-
-snapshots = sorted(snapshots, key=attrgetter("time"))
-
-Path("frames").mkdir()
-
-batch_size = 100
-total_blocks = (len(snapshots) // batch_size) + 1
-for i in range(total_blocks):
-    log.info(f"Running block {i+1} of {total_blocks}")
-    for j, snapshot in enumerate(snapshots[i*100:(i+1)*100]):
-        snapshot: Snapshot
-        image = Image.frombytes("RGB", (MAX_WIDTH, MAX_HEIGHT), bytes(snapshot.pixels))
-        image = image.resize((1600, 900), Image.NEAREST)
-        image.save(f"timelapse_output/frames/frame{j}.png", format="png")
-log.info(f"Transformation done. Took {perf_counter()-start:.2f}s")
-
-log.info("Saving frames to a mp4...")
+log.info("Rendering frames to a mp4...")
 start = perf_counter()
 os.system(
     "ffmpeg "
