9156a43a0b85be638b2325eb62d87d35b8fd3b85
ChrisJL <ChrisLovering@users.noreply.github.com>
2021-06-19 11:10:42 +0100

    Use ternary operators in gunicorn_conf
    Co-authored-by: Bluenix <bluenixdev@gmail.com>

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,55 +1,31 @@
 # This code is sourced from https://github.com/tiangolo/uvicorn-gunicorn-docker
 # There isn't a 3.9.5 image yet, so we had to make our own
-
 import json
 import multiprocessing
 import os
 
-workers_per_core_str = os.getenv("WORKERS_PER_CORE", "1")
-max_workers_str = os.getenv("MAX_WORKERS")
-use_max_workers = None
-if max_workers_str:
-    use_max_workers = int(max_workers_str)
-web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
+use_max_workers = int(os.getenv("MAX_WORKERS", "0"))
+web_concurrency = int(os.getenv("WEB_CONCURRENCY", "0"))
+workers_per_core = float(os.getenv("WORKERS_PER_CORE", "1"))
+if not web_concurrency:
+    web_concurrency = max(int(workers_per_core * multiprocessing.cpu_count()), 2)
+    if use_max_workers:
+        web_concurrency = min(web_concurrency, use_max_workers)
 
 host = os.getenv("HOST", "0.0.0.0")
 port = os.getenv("PORT", "80")
-bind_env = os.getenv("BIND", None)
-use_loglevel = os.getenv("LOG_LEVEL", "info")
-if bind_env:
-    use_bind = bind_env
-else:
-    use_bind = f"{host}:{port}"
-
-cores = multiprocessing.cpu_count()
-workers_per_core = float(workers_per_core_str)
-default_web_concurrency = workers_per_core * cores
-if web_concurrency_str:
-    web_concurrency = int(web_concurrency_str)
-    assert web_concurrency > 0
-else:
-    web_concurrency = max(int(default_web_concurrency), 2)
-    if use_max_workers:
-        web_concurrency = min(web_concurrency, use_max_workers)
-accesslog_var = os.getenv("ACCESS_LOG", "-")
-use_accesslog = accesslog_var or None
-errorlog_var = os.getenv("ERROR_LOG", "-")
-use_errorlog = errorlog_var or None
-graceful_timeout_str = os.getenv("GRACEFUL_TIMEOUT", "120")
-timeout_str = os.getenv("TIMEOUT", "120")
-keepalive_str = os.getenv("KEEP_ALIVE", "5")
+bind = os.getenv("BIND") or f"{host}:{port}"
 
 # Gunicorn config variables
-loglevel = use_loglevel
 workers = web_concurrency
-bind = use_bind
-errorlog = use_errorlog
-worker_tmp_dir = "/dev/shm"
-accesslog = use_accesslog
-graceful_timeout = int(graceful_timeout_str)
-timeout = int(timeout_str)
-keepalive = int(keepalive_str)
 
+loglevel = os.getenv("LOG_LEVEL", "info")
+errorlog = os.getenv("ERROR_LOG")
+accesslog = os.getenv("ACCESS_LOG")
+
+graceful_timeout = int(os.getenv("GRACEFUL_TIMEOUT", "120"))
+timeout = int(os.getenv("TIMEOUT", "120"))
+keepalive = int(os.getenv("KEEP_ALIVE", "5"))
 
 # For debugging and testing
 log_data = {
